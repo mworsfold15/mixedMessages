5ebfec8168c21ff51704bc5fcd7994c90217cb8a
Matt Worsfold <mworsfold15@gmail.com>
2021-03-28 12:15:19 +0100

    added title to object version

--- mixedMessages.js
+++ mixedMessages.js
@@ -1,4 +1,5 @@
-//array of inspirational quotes
+//object version
+
 const inspirationalQuotes = ['There is no triumph without struggle.', 'Don\'t count the days. Make the days count.', 'Push yourself, because no one else will do it for you.'];
 
 
